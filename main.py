8de94c65968731d5a42404c4e5ba02e36a4c67bd
FathanAlfariel <faril191204@gmail.com>
2024-12-08 15:01:57 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+query = input("Query:")
+response = model.generate_content(query)
+print(response.text)
