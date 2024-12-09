1ca7f6241be5d8388a45dd14a20e63e8bf8ab791
FathanAlfariel <faril191204@gmail.com>
2024-12-09 12:34:51 +0700

    first commit

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-query = input("Query:")
-response = model.generate_content(query)
-print(response.text)
+while True:
+    prompt = input("prompt (ketik 'quit' untuk keluar): ")
+    if prompt.lower() == 'quit':
+        print("Keluar dari program")
+        break
+    try:
+        response = model.generate_content(prompt)
+        print(f"Response: {response.text}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
